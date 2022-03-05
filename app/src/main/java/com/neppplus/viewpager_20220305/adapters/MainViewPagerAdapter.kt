148333721e25a340d57f689af84fd91e8d5d8b1b
skyoo1226 <you1226@naver.com>
2022-03-05 15:10:07 +0900

    탭레이아웃의 색상 변경

--- app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,18 @@ import com.neppplus.viewpager_20220305.fragments.MyNameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentStatePagerAdapter( fm ) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        if ( position == 0) {
+            return "이름"
+        }
+        else if ( position == 1) {
+            return "정보"
+        }
+        else {
+            return "인사말"
+        }
+    }
+
     override fun getCount(): Int {
         return 3
     }
